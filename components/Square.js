d9edda7688a8b8885ebe05ecf6daeca5f573909d
Ewerton Assis <earaujoassis@gmail.com>
2018-05-03 18:36:35 -0300

    Add animation for transitions

--- components/Square.js
+++ components/Square.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { StyleSheet, TouchableWithoutFeedback, View } from 'react-native';
+import { StyleSheet, Animated, TouchableWithoutFeedback, View } from 'react-native';
 
 const COLORS = ['black', 'blue', 'cyan', 'green', 'magenta', 'red', 'yellow'];
+
+const getColor = (i) => COLORS[i % 7];
 
 export default class Row extends React.Component {
   constructor(props) {
@@ -10,22 +12,26 @@ export default class Row extends React.Component {
     this.state = {
       index: props.index,
     };
+    this.animation = new Animated.Value(props.index % 7);
+    this.backgroundColorConfig = this.animation.interpolate({
+        inputRange: [0, 1, 2, 3, 4, 5, 6],
+        outputRange: COLORS,
+    });
   }
 
-  componentWillMount() {
-
-  }
+    startBackgroundColorAnimation() {
+        let nextIndex = this.state.index + 1 > 6 ? 0 : this.state.index + 1;
+        Animated
+            .timing(this.animation, {toValue: nextIndex, duration: 250})
+            .start(() => this.setState({index: nextIndex }));
+    }
 
   render() {
     return (
-      <TouchableWithoutFeedback onPress={() => this.setState({index: this.state.index + 1})}>
-        <View style={[styles.square, {backgroundColor: this.getNextColor()}]} />
+      <TouchableWithoutFeedback onPress={() => this.startBackgroundColorAnimation()}>
+        <Animated.View style={[styles.square, {backgroundColor: this.backgroundColorConfig}]} />
       </TouchableWithoutFeedback>
     );
-  }
-
-  getNextColor() {
-    return COLORS[this.state.index % 7];
   }
 }
 
